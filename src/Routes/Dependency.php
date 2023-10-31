4fefd39e65bfe89fe9f6b0dba8b77d307eef2652
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2023-10-31 09:26:34 +0600

    add args for dependency route

--- src/Routes/Dependency.php
+++ src/Routes/Dependency.php
@@ -16,8 +16,8 @@ class Dependency extends RouteBase {
 	private $endpoint = 'dependency';
 
 	public function register_routes(): void {
-		$this->post( $this->endpoint . '/check', [$this, 'check'] );
-		$this->post( $this->endpoint . '/install', [$this, 'install'] );
+		$this->post( $this->endpoint . '/check', [$this, 'check'], $this->prepare_args( 'check' ) );
+		$this->post( $this->endpoint . '/install', [$this, 'install'], $this->prepare_args( 'install' ) );
 	}
 
 	public function check() {
@@ -45,5 +45,37 @@ class Dependency extends RouteBase {
 		}
 
 		return Plugin::installer( $plugin );
+	}
+
+	private function prepare_args( string $type ) {
+		$args = [
+			'plugins' => [
+				'description' => 'Send the list of plugins array',
+				'type'        => 'array',
+				'required'    => true,
+			],
+			'plugin'  => [
+				'description' => 'Send the plugin array',
+				'type'        => 'array',
+				'required'    => true,
+			],
+		];
+
+		$_args = [];
+		switch ( $type ) {
+			case 'check':
+				$_args = [
+					'plugins' => $args['plugins'],
+				];
+				break;
+
+			case 'install':
+				$_args = [
+					'plugin' => $args['plugin'],
+				];
+				break;
+		}
+
+		return $_args;
 	}
 }
