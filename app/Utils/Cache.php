56d0dff7f211a1cac42c82373ca2452f358a1f47
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2023-11-16 12:57:36 +0600

    template data caching added

---
+++ app/Utils/Cache.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author  wpWax
+ * @since   1.0.0
+ * @version 1.0.0
+ */
+
+namespace Templatiq\Utils;
+
+class Cache {
+
+	public static function get( array $args ): array {
+		$key  = self::key( ...$args );
+		$data = Transient::get( $key );
+
+		if ( $data ) {
+			return $data;
+		}
+
+		return [];
+	}
+
+	private function key( string...$items ): string {
+		$single = "demo_data_";
+
+		foreach ( $items as $item ) {
+			$single .= $item . "_";
+		}
+
+		return trim( $single );
+	}
+
+	public static function set( array $args, array $data ): void {
+		$key = self::key( ...$args );
+		Transient::set( $key, $data, 0 );
+	}
+}
