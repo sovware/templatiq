0dfced5b2fb5b0e1d4f1c29aa76260b778ad9cb6
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2023-11-20 18:32:10 +0600

    removed user login

--- app/Routes/Account.php
+++ app/Routes/Account.php
@@ -15,9 +15,9 @@ class Account extends RouteBase {
 	private $endpoint = 'account';
 
 	public function register_routes(): void {
-		$this->post( $this->endpoint . '/login', [$this, 'login'], $this->prepare_args( 'login' ) );
+		$this->post( $this->endpoint . '/login', [$this, 'login'] );
 		$this->post( $this->endpoint . '/logout', [$this, 'logout'] );
-		$this->post( $this->endpoint . '/create', [$this, 'create'], $this->prepare_args( 'create' ) );
+		$this->post( $this->endpoint . '/create', [$this, 'create'] );
 		$this->get( $this->endpoint . '/data', [$this, 'data'] );
 		$this->get( $this->endpoint . '/is-logged-in', [$this, 'is_logged_in'] );
 		$this->post( $this->endpoint . '/directorist-sync', [$this, 'directorist_membership_sync'] );
@@ -41,11 +41,11 @@ class Account extends RouteBase {
 	}
 
 	public function create() {
-		$user_login = $this->get_param( 'user_login', '', 'sanitize_user' );
+		$name       = $this->get_param( 'name', '', 'sanitize_text_field' );
 		$user_email = $this->get_param( 'user_email', '', 'sanitize_email' );
 
 		$account  = new ModelAccount;
-		$response = $account->create( $user_login, $user_email );
+		$response = $account->create( $user_email, $name );
 
 		return $response;
 	}
@@ -69,71 +69,5 @@ class Account extends RouteBase {
 		$response    = $directorist->sync_membership_with_cloud();
 
 		return $response;
-	}
-
-	private function prepare_args( string $type ) {
-		$args = [
-			'username'         => [
-				'description' => 'Cloud site username',
-				'type'        => 'string',
-				'required'    => false,
-			],
-			'password'         => [
-				'description' => 'Cloud site password',
-				'type'        => 'string',
-				'required'    => false,
-			],
-			'token'            => [
-				'description' => 'JWT token, user can collect it from our cloud website account area.',
-				'type'        => 'string',
-				'required'    => false,
-			],
-			'additional'       => [
-				'description' => 'This option required when you sending authentication request using JWT.',
-				'type'        => 'bool',
-				'required'    => false,
-				'default'     => false,
-			],
-			'first_name'       => [
-				'type'     => 'string',
-				'required' => false,
-			],
-			'last_name'        => [
-				'type'     => 'string',
-				'required' => false,
-			],
-			'email'            => [
-				'type'     => 'string',
-				'required' => false,
-			],
-			'confirm_password' => [
-				'type'     => 'string',
-				'required' => false,
-			],
-		];
-
-		$_args = [];
-		switch ( $type ) {
-			case 'login':
-				$_args = [
-					'username'   => $args['username'],
-					'password'   => $args['password'],
-					'token'      => $args['token'],
-					'additional' => $args['additional'],
-				];
-				break;
-
-			case 'create':
-				$_args = [
-					'first_name'       => $args['first_name'],
-					'last_name'        => $args['last_name'],
-					'email'            => $args['email'],
-					'password'         => $args['password'],
-					'confirm_password' => $args['confirm_password'],
-				];
-				break;
-		}
-
-		return $_args;
 	}
 }
