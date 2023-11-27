bb7a0d61e592a24d96899085b142d8da75f5cb35
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2023-11-27 11:13:03 +0600

    Bookmark - Updated

--- src/js/components/Bookmark/index.js
+++ src/js/components/Bookmark/index.js
@@ -7,8 +7,9 @@ import store from '../../store';
 import heartIcon from "@icon/heart.svg";
 import heartSolidIcon from "@icon/heart-solid.svg";
 
-const Bookmark = ( props ) => {
+const Bookmark = ( props) => {
     let { template_id, number_of_bookmarks } = props.item;
+    const [ type, setType ] = useState(props.type ? props.type : '');
 
 	const { isLoggedIn } = select( store ).getUserInfo();
     const favCountList = select( store ).getFav(template_id);
@@ -41,6 +42,7 @@ const Bookmark = ( props ) => {
 
             setCurrentFavoriteCount(updatedCount)
 
+            props.onFavoriteCountUpdate?.();
             // Return the new value to update the state
             return newAddedToFavorite;
         });
@@ -56,22 +58,25 @@ const Bookmark = ( props ) => {
         setCurrentFavoriteCount(favCountList);
     }, []);  // Add favCountList to the dependency array
 
-
     return (
         <>
             {
-                !isLoggedIn ?
-                <>
-                    {authModalOpen ? <AuthModal modalEnable={true} onClose={handleAuthModalClose} /> : ''}
-                    <a href="#" className='templatiq__template__single__quickmeta__item favorite-btn templatiq-tooltip' data-info='Add to Favourite' onClick={addAuthModal}>
-                        <ReactSVG src={ heartIcon } width={14} height={14} />
-                        {favCountList ? favCountList : ''}
-                    </a>
-                </> :
-                <a href="#" className={`templatiq__template__single__quickmeta__item favorite-btn templatiq-tooltip ${addedToFavorite ? 'active' : ''}`} data-info={addedToFavorite ? 'Added to Favourite' : 'Add to Favourite'} onClick={handleFavorite}>
-                    <ReactSVG src={ addedToFavorite ? heartSolidIcon : heartIcon } width={14} height={14} />
-                    {currentFavoriteCount ? currentFavoriteCount : ''}
-                </a>
+                type === 'single' ? 
+                    <a href="#" className={`templatiq__details__header__action__link add-to-favorite templatiq-btn templatiq-btn-white ${addedToFavorite ? 'active' : ''}`} onClick={(e) => handleFavorite(e, number_of_bookmarks)}>
+                        <ReactSVG src={ addedToFavorite ? heartSolidIcon : heartIcon } width={16} height={16} />
+                    </a> :
+                    !isLoggedIn ?
+                        <>
+                            {authModalOpen ? <AuthModal modalEnable={true} onClose={handleAuthModalClose} /> : ''}
+                            <a href="#" className='templatiq__template__single__quickmeta__item favorite-btn templatiq-tooltip' data-info='Add to Favourite' onClick={addAuthModal}>
+                                <ReactSVG src={ heartIcon } width={14} height={14} />
+                                {favCountList ? favCountList : ''}
+                            </a>
+                        </> :
+                        <a href="#" className={`templatiq__template__single__quickmeta__item favorite-btn templatiq-tooltip ${addedToFavorite ? 'active' : ''}`} data-info={addedToFavorite ? 'Added to Favourite' : 'Add to Favourite'} onClick={handleFavorite}>
+                            <ReactSVG src={ addedToFavorite ? heartSolidIcon : heartIcon } width={14} height={14} />
+                            {currentFavoriteCount ? currentFavoriteCount : ''}
+                        </a>
             }
         </>
     )
