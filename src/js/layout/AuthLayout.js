82e408f3a37ad633646036384586eecc1ba93d11
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2023-12-26 16:41:39 +0600

    Route Change Based on Login Status - Updated

--- src/js/layout/AuthLayout.js
+++ src/js/layout/AuthLayout.js
@@ -1,6 +1,19 @@
+import { useEffect } from '@wordpress/element';
+import { select } from '@wordpress/data';
+import { useNavigate } from 'react-router-dom';
 import Header from '@layout/Header';
+import store from '@store/index';
+
 
 const AuthLayout = ({ children }) => {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const { isLoggedIn } = select( store ).getUserInfo();
+		if (isLoggedIn) {
+			navigate('/dashboard/favorites');
+		}
+    }, []);
 
 	return (
 		<div className="templatiq">
