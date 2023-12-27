52942cd967b2a6198fb2c72c76cbbe8d27fc2893
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2023-12-27 13:14:39 +0600

    Removed Unused Icons

--- src/js/hooks/usePostMutation.js
+++
@@ -1,8 +0,0 @@
-import { useMutation } from '@tanstack/react-query';
-import postData from '@helper/postData.js';
-
-const usePostMutation = ( path, config ) => {
-	return useMutation( ( data ) => postData( path, data, config ) );
-};
-
-export default usePostMutation;
