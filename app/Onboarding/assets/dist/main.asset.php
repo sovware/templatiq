622f28e6d5ed76559a88f2c3a45171b881cda6a6
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2024-01-01 15:01:32 +0600

    Updaetd

--- app/Onboarding/assets/dist/main.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => '38e4722ec1a29d6e30da');
