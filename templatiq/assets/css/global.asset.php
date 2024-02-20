0959b51bfe8901735b816badebaf307ca4323993
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2024-02-20 15:38:55 +0600

    add release tag compress

--- templatiq/assets/css/global.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '7253678328952001db1e');
--- templatiq/assets/js/admin.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-hooks'), 'version' => '82fdf8819edca6229800');
