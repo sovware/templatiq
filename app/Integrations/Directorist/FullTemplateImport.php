526ec1cc95490ca9e420439f0b6f9985d9708d83
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2024-09-15 14:00:29 +0600

    fix: duplicate directory types

--- app/Integrations/Directorist/FullTemplateImport.php
+++ app/Integrations/Directorist/FullTemplateImport.php
@@ -49,17 +49,36 @@ class FullTemplateImport {
 				}
 			}
 		}
+
+		// Directory Type update
+		$types = get_terms( ['taxonomy' => 'atbdp_listing_types', 'fields' => 'ids'] ); // phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_key
+		if ( ! empty( $types ) ) {
+			$uuid = 1;
+			foreach ( $types as $type ) {
+				if ( 1 === $uuid ) {
+					update_term_meta( $type, '_default', $attachment_id );
+					$uuid++;
+				}
+			}
+		}
 	}
 
 	public function set_directory_type_term( $term_id, $data ) {
 		if ( isset( $data['taxonomy'] ) && ( 'at_biz_dir-location' === $data['taxonomy'] || 'at_biz_dir-category' === $data['taxonomy'] ) ) {
-			// $_pre_types = get_term_meta( $term_id, '_directory_type', true );
-			// error_log( '$_pre_types: ' . print_r( $_pre_types, true ) );
-
+			// Get the directory type mappings.
 			$types = get_option( 'templatiq_sites_directory_types_ids_mapping', [] );
-			if ( $types ) {
+
+			if ( ! empty( $types ) ) {
+				// Update term meta with directory types.
 				update_term_meta( $term_id, '_directory_type', array_values( $types ) );
-				// wp_set_object_terms( $term_id, $value, 'atbdp_listing_types' );
+
+				// Define a term or terms to associate with the 'atbdp_listing_types' taxonomy.
+				$value = array_values( $types ); // Assuming $types is an array of term IDs or slugs for 'atbdp_listing_types'.
+
+				// Ensure $value is not empty before setting terms.
+				if ( ! empty( $value ) ) {
+					wp_set_object_terms( $term_id, $value, 'atbdp_listing_types' );
+				}
 			}
 		}
 	}
@@ -75,7 +94,7 @@ class FullTemplateImport {
 				update_post_meta( $post_id, '_directory_type', $value );
 				wp_set_object_terms( $post_id, $value, 'atbdp_listing_types' );
 
-				// error_log( 'Directory Type Changed - post_id #' . $post_id . ' from #' . $meta_item['value'] . ' => ' . $value );
+				error_log( 'Directory Type Changed - post_id #' . $post_id . ' from #' . $meta_item['value'] . ' => ' . $value );
 			}
 		}
 
