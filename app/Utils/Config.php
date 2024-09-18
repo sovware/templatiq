e765461404ce18c68bbe025efb94781d63bcfe03
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2024-09-18 18:11:18 +0600

    version update 0.0.5

--- app/Utils/Config.php
+++ app/Utils/Config.php
@@ -11,7 +11,7 @@ class Config {
 
 	public static function get( string $name ): string {
 		$config = [
-			'version'        => '0.0.4',
+			'version'        => '0.0.5',
 			'environment'    => 'production',
 			'could_base'     => 'https://templatiq.com/',
 		];
