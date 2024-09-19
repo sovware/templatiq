e4bc2fba351d8d654978fcb4f261dd79277d03c4
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2024-09-19 15:22:43 +0600

    block builder import as page complete

--- app/Repositories/BlockBuilderRepository.php
+++ app/Repositories/BlockBuilderRepository.php
@@ -10,8 +10,12 @@ namespace Templatiq\Repositories;
 use Templatiq\DTO\ImportAsPageDTO;
 
 class BlockBuilderRepository {
+	const IMAGE_URL_PATTERN = '/(http(s?):)([\/|.|\w|\s|-])*\.(?:jpg|gif|png)/';
+
 	public function create_page( ImportAsPageDTO $DTO ) {
+
 		try {
+
 			$template_content = $DTO->get_template_data()['content'] ?? '';
 
 			$inserted_id = wp_insert_post( [
@@ -28,14 +32,14 @@ class BlockBuilderRepository {
 				);
 			}
 
-			$processed_content = $this->process_images( $template_content, $inserted_id );
+			// $processed_content = $this->process_images( $template_content, $inserted_id );
 
-			$updated_post = [
-				'ID'           => $inserted_id,
-				'post_content' => wp_slash( $processed_content['content'] ),
-			];
+			// $updated_post = [
+			// 	'ID'           => $inserted_id,
+			// 	'post_content' => wp_slash( $processed_content['content'] ),
+			// ];
 
-			wp_update_post( $updated_post );
+			// wp_update_post( $updated_post );
 
 			return $inserted_id;
 
@@ -46,10 +50,10 @@ class BlockBuilderRepository {
 
 	public function process_images( $template_content, $postId = 0 ) {
 		// Instantiate GutenbergHelper
-		$helper = new GutenbergHelper();
+		$helper = '';
 
 		// Organize URLs from the content
-		$organizedUrls = $helper->parse_images( $template_content );
+		$organizedUrls = $this->parse_images( $template_content );
 
 		// Define template settings
 		$template_settings = [
@@ -57,15 +61,39 @@ class BlockBuilderRepository {
 			'__attachments' => $organizedUrls,
 		];
 
-		// Map post IDs and disable logging
-		$helper->map_post_ids[$postId] = $postId;
-		$helper->shouldLog             = false;
-
 		// Prepare the helper with the data and settings
 		$helper->prepare( $template_content, $template_settings );
 
 		// Update the content in the data array
 
 		return wp_unslash( $helper->get_content() );
+	}
+
+	/**
+	 * Parses the images from the post content, processes them as attachments, and updates the post with the new attachment URL.
+	 * @todo: Parse using parse_blocks instead of regex
+	 *
+	 * @param int    $post_id      The ID of the post.
+	 * @param string $post_content The content of the post.
+	 */
+	public function parse_images( $post_content ) {
+		// Find all image URLs in the post content
+		preg_match_all( self::IMAGE_URL_PATTERN, $post_content, $matches );
+
+		$urls          = $matches[0];
+		$organizedUrls = [];
+
+		foreach ( $urls as $url ) {
+			// Remove the size suffix from the URL
+			$base_url = preg_replace( '/-\d+x\d+(?=\.[a-zA-Z]+$)/', '', $url );
+
+			// Check if the URL already exists in the array
+			if ( ! isset( $organizedUrls[$base_url] ) || ! in_array( $url, $organizedUrls[$base_url] ) ) {
+				// Add the URL to the array under the base URL key
+				$organizedUrls[$base_url][] = $url;
+			}
+		}
+
+		return $organizedUrls;
 	}
 }
