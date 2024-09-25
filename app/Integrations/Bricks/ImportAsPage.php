d1cc273895f4d3a93ad61ee94b926dda86f4eb76
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2024-09-25 16:14:33 +0600

    bricks single page importing complete

---
+++ app/Integrations/Bricks/ImportAsPage.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @author  wpWax
+ * @since   1.0.0
+ * @version 1.0.0
+ */
+
+namespace Templatiq\Integrations\Bricks;
+
+use Templatiq\DTO\ImportAsPageDTO;
+
+class ImportAsPage {
+	use \Templatiq\Utils\Singleton;
+
+	private string $cloud_endpoint;
+
+	public function __construct() {
+		add_filter( 'templatiq_import_as_page_created_post_id', [$this, 'import_as_page'], 10, 2 );
+		add_filter( 'templatiq_before_return_import_as_page', [$this, 'before_return_import_as_page'], 10, 2 );
+
+	}
+
+	public function import_as_page( $page_id, ImportAsPageDTO $DTO ) {
+
+		if ( 'bricks' !== $DTO->get_builder() ) {
+			return;
+		}
+
+		$template_data = $DTO->get_template_data();
+
+		$_content       = $template_data['content'] ?? [];
+		$_title         = $template_data['title'] ?? '';
+		$_post_title    = $DTO->get_title() ?? 'Templatiq: ' . $_title;
+		$_type          = $template_data['type'] ?? '';
+		$_status        = current_user_can( 'publish_posts' ) ? 'publish' : 'pending';
+		$_page_settings = $template_data['page_settings'] ?? [];
+
+		$templateDataDTO = ( new TemplateDataDTO )
+			->set_content( $_content )
+			->set_title( $_post_title )
+			->set_type( $_type )
+			->set_status( $_status )
+			->set_page_settings( $_page_settings );
+
+		return ( new Repository )->create_page( $templateDataDTO );
+	}
+
+	public function before_return_import_as_page( $data, ImportAsPageDTO $DTO ) {
+		if ( 'bricks' === $DTO->get_builder() ) {
+			$post_id           = $data['post_id'] ?? 0;
+			$data['edit_link'] = $this->get_edit_url( $post_id );
+		}
+
+		return $data;
+	}
+
+	private function get_edit_url( int $post_id ) {
+		return add_query_arg( 'bricks', 'run', get_permalink( $post_id ) );
+	}
+}
---
+++ app/Integrations/Bricks/Repository.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @author  wpWax
+ * @since   1.0.0
+ * @version 1.0.0
+ */
+
+namespace Templatiq\Integrations\Bricks;
+
+use Bricks\Helpers;
+use Bricks\Templates;
+
+class Repository {
+	private string $cloud_endpoint;
+
+	public function __construct() {
+		$this->cloud_endpoint = TEMPLATIQ_CLOUD_API_BASE;
+	}
+
+	public function create_page( TemplateDataDTO $template_data ): int {
+
+		$inserted_id = wp_insert_post( [
+			'post_status' => $template_data->get_status(),
+			'post_type'   => 'page',
+			'post_title'  => $template_data->get_title(),
+		] );
+
+		if ( is_wp_error( $inserted_id ) ) {
+			throw new \Exception(
+				esc_html( $inserted_id->get_error_message() ),
+				esc_html( $inserted_id->get_error_code() )
+			);
+		}
+
+		$data = $this->replace_images( $template_data->get_content() );
+
+		update_post_meta( $inserted_id, BRICKS_DB_PAGE_CONTENT, $data );
+
+		return $inserted_id;
+	}
+
+	/**
+	 * Code inspired from Bricks
+	 */
+	private function replace_images( $elements ) {
+
+		foreach ( $elements as $index => $element ) {
+			if ( empty( $element['settings'] ) ) {
+				$elements[$index]['settings'] = [];
+			} else {
+				// STEP: Import element images & update template data with local image data
+				Templates::import_images( $element['settings'], true );
+			}
+
+			if ( ! isset( $element['children'] ) ) {
+				$elements[$index]['children'] = [];
+			}
+		}
+
+		// STEP: Replace remote image data with imported/existing image data
+		if ( count( Templates::$template_images ) ) {
+			$elements_encoded = wp_json_encode( $elements );
+
+			foreach ( Templates::$template_images as $template_image ) {
+				$elements_encoded = str_replace(
+					wp_json_encode( $template_image['old'] ),
+					wp_json_encode( $template_image['new'] ),
+					$elements_encoded
+				);
+			}
+
+			$elements = json_decode( $elements_encoded, true );
+		}
+
+		// STEP: Generate new element IDs (@since 1.9.9)
+
+		return Helpers::generate_new_element_ids( $elements );
+	}
+}
