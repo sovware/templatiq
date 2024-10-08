d8cd9c155cdb4a98976613e538119e2deeb0e2b6
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2024-10-08 10:05:49 +0600

    text update

--- src/js/onboarding/steps/insert-content/index.js
+++ src/js/onboarding/steps/insert-content/index.js
@@ -292,7 +292,7 @@ const InsertContent = () => {
 														width={ 16 }
 														height={ 16 }
 													/>
-													<strong>Warning: </strong> This action will erase all existing directories, listings, and related data from Directorist.
+													<strong>Warning: </strong> This will remove all existing data from your site.
 												</span>
 											}
 										</>
