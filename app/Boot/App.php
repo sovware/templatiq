f7840cc0899892ac8508f4773209bfc8450535ac
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2024-10-26 13:48:54 +0600

    return after successful purchase method added

--- app/Boot/App.php
+++ app/Boot/App.php
@@ -56,6 +56,7 @@ class App {
 		define( 'TEMPLATIQ_CLOUD_BASE', Config::get( 'could_base' ) );
 		define( 'TEMPLATIQ_CLOUD_API_BASE', TEMPLATIQ_CLOUD_BASE . 'wp-json/cloud' );
 		define( 'TEMPLATIQ_CLOUD_CONNECT_URL', TEMPLATIQ_CLOUD_BASE . 'login/' );
+		define( 'TEMPLATIQ_CLOUD_CHECKOUT_URL', TEMPLATIQ_CLOUD_BASE . 'checkout/' );
 
 		define( 'TEMPLATIQ_DEV', true );
 		define( 'TEMPLATIQ_DEBUG_LOG', true );
