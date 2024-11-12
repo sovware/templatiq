539a0ef89275c9a1fd681a81840ede82b25f3cf9
Ibrahim Riaz <ibrahimriaz.sovware@gmail.com>
2024-11-12 10:21:08 +0600

    Updated

--- src/js/helper/sanitizeHtmlEntities.js
+++ src/js/helper/sanitizeHtmlEntities.js
@@ -1,6 +1,5 @@
 // Sanitize HTML Entities
 export default function sanitizeHtmlEntities(text) {
-	console.log('sanitizeHtmlEntities', text);
 	return text
 		.replace(/&#8211;/g, '–')
 		.replace(/&amp;/g, '&');
